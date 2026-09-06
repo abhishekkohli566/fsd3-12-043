c240c754c5ce3b3bc88d4001c4a7720bb19d2de8
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-09-06 07:57:23 +0530

    server run nodemon

--- lab3/prg5.js
+++ lab3/prg5.js
@@ -1,10 +1,13 @@
 import http from 'http'
 
 const server = http.createServer((req, res) => {
-
-if(req.url=="/")
+if(req.url=="/"){
     res.end("<h1> Home page</h1>");
-else if(req.ur=="/product"){
+   res.end(`
+    <a href='/product'>Product page</a>
+    <a href='/contact'>Conactt us</a>
+    `);
+   } else if(req.ur=="/product"){
     res.write(`
         <h1> Iphone 18 pro</h1>
         <h2> Price: 90000</h2>
