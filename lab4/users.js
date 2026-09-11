35ad1bb8bc55d4d9519eb9a001643578e1611054
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-09-11 16:17:39 +0530

    users updates

---
+++ lab4/users.js
@@ -0,0 +1,8 @@
+//we use in memory database
+let users=[
+{ id:1,name:'Abhishek kohli',mob:'9219264188',email:'abhishekkohli566@gmail.com'},
+{ id:2,name:'aayush sharma',mob:'7055829486',email:'aayush.4723@gmail.com'}
+]
+let nextId=3;
+
+export const getUsers =() =>users
