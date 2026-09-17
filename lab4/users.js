9dc1dc0ed0e9f50bd00dd6ac31c6c1d6f2de36ca
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-09-17 12:22:38 +0530

    export getusers and add users

--- lab4/users.js
+++ lab4/users.js
@@ -2,9 +2,14 @@
 let users=[
 { id:1,name:'Abhishek kohli',mob:'9219264188',email:'abhishekkohli566@gmail.com'},
 { id:2,name:'aayush sharma',mob:'7055829486',email:'aayush.4723@gmail.com'},
-{ id:3,name:'nipun sharma',mob:'9911288116',email:'nipunsharma7102005@gmail.com'},
-{ id:4,name:'mukul',mob:'8950380944',email:'muukaushik792@gmail.com'}
+
 ]
 let nextId=3;
 
-export const getUsers =() =>users
+export const getUsers =() =>users;
+
+export const addUsers =(user) =>{
+    user.id=nextId++;
+    users.push(user);
+    return user;
+};
