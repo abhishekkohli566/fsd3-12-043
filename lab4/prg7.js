4f2ed930a8b384159184f8f663c24609b3d3533d
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-09-17 12:16:15 +0530

    add user created

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,12 +1,23 @@
 import http from "http";
-import { getUsers } from "./users.js";
+import { addUsers, getUsers } from "./users.js";
 
 const server = http.createServer((req, res) => {
+
   if (req.url === "/api/users" && req.method === "GET") {
     res.end(JSON.stringify(getUsers()));
   } else if (req.url === "/api/users" && req.method === "POST") {
-    res.end(JSON.stringify({ msg: "add users" }));
+    let body ='';
+    req.on('data',(chunk)=>{
+      body += chunk;
+    });
+    req.on("end", () => {
+      const user =JSON.parse(body);
+      const userCreated=addUsers(user);
+       res.end(JSON.stringify({ msg: "user added",userCreated }));
+    });
+   
   } else if (req.url === "/api/users/1" && req.method == "GET") {
+    // add user crated
     res.end(JSON.stringify({ msg: "single user with id 1" }));
   } else if (req.url === "/api/users/1" && req.method === "PUT") {
     res.end(JSON.stringify({ msg: "update user 1" }));
