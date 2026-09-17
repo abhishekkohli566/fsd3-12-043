f4cd326c8b19ef87bb1b07362b4cd86d9d09ad58
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-09-17 12:21:48 +0530

    add user and get user by id created

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -16,9 +16,12 @@ const server = http.createServer((req, res) => {
        res.end(JSON.stringify({ msg: "user added",userCreated }));
     });
    
-  } else if (req.url === "/api/users/1" && req.method == "GET") {
-    // add user crated
-    res.end(JSON.stringify({ msg: "single user with id 1" }));
+  } else if (req.url.startsWith( "/api/users/") && req.method == "GET") {
+   const userId =Number(req.url.split('/').pop())
+    
+    res.end(JSON.stringify({ msg: `Showing detail of user with id  ${userId
+        
+    }` }));
   } else if (req.url === "/api/users/1" && req.method === "PUT") {
     res.end(JSON.stringify({ msg: "update user 1" }));
   } else if (req.url === "/api/users/1" && req.method === "DELETE") {
