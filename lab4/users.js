01657ad43a276ace41ad2b8f8157a7e2c6a7a814
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-09-24 13:12:54 +0530

    export all

--- lab4/users.js
+++ lab4/users.js
@@ -1,15 +1,47 @@
 //we use in memory database
 let users=[
-{ id:1,name:'Abhishek kohli',mob:'9219264188',email:'abhishekkohli566@gmail.com'},
-{ id:2,name:'aayush sharma',mob:'7055829486',email:'aayush.4723@gmail.com'},
+{ id:1,
+    name:'Abhishek kohli',
+    mob:'9219264188',
+    email:'abhishekkohli566@gmail.com'},
+{ id:2,
+    name:'aayush sharma',
+    mob:'7055829486',
+    email:'aayush.4723@gmail.com'},
 
 ]
 let nextId=3;
 
-export const getUsers =() =>users;
+
+ export const getAllUsers = () =>{
+    return users;
+}
+ export const getUsersById = (pid) =>{
+    const found= users.find((user)=> user.id==pid)
+    return found;
+}
 
 export const addUsers =(user) =>{
     user.id=nextId++;
     users.push(user);
     return user;
 };
+
+ export const updateUsers =(pid,updateData) =>{
+    const index= users.findIndex((user)=> user.id ===pid);
+    if(index == -1){
+        return false;
+    }
+    updateData.id =pid;
+    users[index]=updateData;
+    return updateData;
+
+}
+
+export const deleteUser =(pid)=>{
+       const index= users.findIndex((user)=> user.id ===pid);
+    if(index == -1){
+        return false;
+    }
+    users.splice(index,1);
+}
