76998e148cb1a9e3b284ab20f1bc28a0d220e3e5
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-09-26 12:45:36 +0530

    lab 4 complete

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,39 +1,54 @@
 import http from "http";
-import { getAllUsers, getUsersById, updateUsers, addUsers, deleteUser} from "./users.js";
+import {
+  addUser,
+  getAllUsers,
+  getUserById,
+  udpateUser,
+  deleteUser,
+} from "./users.js";
 
 const server = http.createServer((req, res) => {
-
   if (req.url === "/api/users" && req.method === "GET") {
     res.end(JSON.stringify(getAllUsers()));
   } else if (req.url === "/api/users" && req.method === "POST") {
-    let body ='';
-    req.on('data',(chunk)=>{
+    let body = "";
+    req.on("data", (chunk) => {
       body += chunk;
     });
     req.on("end", () => {
-      const user =JSON.parse(body);
-      const userCreated=addUsers(user);
-       res.end(JSON.stringify({ msg: "user added",userCreated }));
+      const user = JSON.parse(body);
+      const userCreated = addUser(user);
+      res.end(JSON.stringify({ msg: "user added", userCreated }));
     });
-   
-  } else if (req.url.startsWith( "/api/users/") && req.method == "GET") {
-   const userId =Number(req.url.split('/').pop())
-    const userFound = getUsersById(userId);
-    if(!userFound){
-      req.end(JSON.stringify({msg: `user not found`}));
-    }else{
-      req.end(JSON.stringify(userFound));
-    }
-    res.end(JSON.stringify({ msg: `Showing detail of user with id  ${userId
-        
-    }` }));
-  } else if (req.url === "/api/users/1" && req.method === "PUT") {
-    res.end(JSON.stringify({ msg: "update user 1" }));
-  } else if (req.url === "/api/users/1" && req.method === "DELETE") {
-    res.end(JSON.stringify({ msg: "remove 1" }));
+  } else if (req.url.startsWith("/api/users/") && req.method === "GET") {
+    const userId = Number(req.url.split("/").pop());
+    const userFound = getUserById(userId);
+    if (!userFound) {
+      res.end(JSON.stringify({ msg: "User not found" }));
+    } else res.end(JSON.stringify(userFound));
+  } else if (req.url.startsWith("/api/users/") && req.method === "PUT") {
+    const userId = Number(req.url.split("/").pop());
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      const user = JSON.parse(body);
+      const userUpdated = udpateUser(userId, user);
+      if (!userUpdated) {
+        res.end(JSON.stringify({ msg: "User not found" }));
+      } else res.end(JSON.stringify(userUpdated));
+    });
+  } else if (req.url.startsWith("/api/users/") && req.method === "DELETE") {
+    const userId = Number(req.url.split("/").pop());
+    const isDeleted = deleteUser(userId);
+    if (!isDeleted) {
+      res.end(JSON.stringify({ msg: "User not found" }));
+    } else res.end(JSON.stringify({ msg: "user deleted" }));
   } else {
     res.statusCode = 404;
     res.end();
   }
 });
-server.listen(3000, () => console.log("prg7 is running..."));
+
+server.listen(3000, () => console.log("prg7 is running"));
